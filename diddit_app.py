409f5d810aa48c9a826d352b29dd9ee2657037c5
Amanda Wright <amanda.jane.wright@gmail.com>
2019-02-18 15:25:36 +0000

    Adding mock api.

--- diddit_app.py
+++ diddit_app.py
@@ -11,7 +11,7 @@ app=Flask("MyApp")
       
 @app.route("/", methods=["GET", "POST"])
 def home():      
-    results = {"id":; "title":;"notes":;"status":;}
+    results = {"id":1, "title":"Buy milk","description":"Make sure has best before of more than two days away.","status":"not done", "priority":"medium", "date":{"start_date":"2019-02-20T00:00:00Z", "end_date":"2019-02-20T00:30:00Z"}}
     return render_template ("index.html", title="results_page", **locals())
 
 app.run(debug=True)
