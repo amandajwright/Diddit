1a805ba39790921a550d5bd901e209bc91170090
Amanda Wright <amanda.jane.wright@gmail.com>
2019-02-20 17:21:58 +0000

    playing with javascript

--- static/js/engine.js
+++ static/js/engine.js
@@ -5,3 +5,38 @@ $('#datepicker').datepicker({
             uiLibrary: 'bootstrap4'
         });
 });
+
+function createNode(element) {
+  return document.createElement(element);
+}
+function append(parent, element) {
+  return parent.appendChild(element);
+}
+
+const ul = document.getElementByID("all_tasks");
+const url = 'http://127.0.0.1:5000/v1/entries/tasks/all'
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) {
+    let tasks = data.results;
+    return tasks.map
+  })
+  .catch(function(error) {
+
+  })
+
+//https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+
+//
+// $.each( response, function( line ) {//loop through lines in response
+//     var keys = [];
+//     var values = [];
+//     $.each( line, function( obj ) {
+//         keys.push( Object.keys(obj) );//get keys
+//         for( var key in obj ) {
+//             values.push(obj[key]);//get values
+//         }
+//     });
+//     lines.push({ {'keys':keys},{'values':values} });
+// });
